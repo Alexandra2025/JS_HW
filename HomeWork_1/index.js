81ad9c0f6f6c8a07189bae1f212ff907cd6ff3ac
Alexandra2025 <turkova2025@gmail.com>
2023-09-19 19:21:11 +0800

    add HomeWork_1

---
+++ HomeWork_1/index.js
@@ -0,0 +1,9 @@
+let celsius = 16;
+console.log(`Температура в градусах Цельсия: ${celsius}`);
+
+let fahrenheit = (9 / 5) * celsius + 32;
+console.log(`Температура в градусах Фаренгейта: ${fahrenheit}`);
+
+let userName = "Alexandra";
+let admin = userName;
+console.log(admin);
